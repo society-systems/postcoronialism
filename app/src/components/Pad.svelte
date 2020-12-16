a02d2a9dc79d44ce89859843d8283d846413d75a
vrde <agranzot@mailbox.org>
2020-12-16 18:13:59 +0100

    Make pad bigger

--- app/src/components/Pad.svelte
+++ app/src/components/Pad.svelte
@@ -1,13 +1,15 @@
 <script>
   export let key;
   export let name;
-  $: {
-    console.log("key is", key);
-  }
 </script>
+
+<style>
+  iframe {
+    height: 100vh;
+  }
+</style>
 
 <iframe
   title="Collaborative pad"
   src="https://pad.riseup.net/p/{key}?userName={name}"
-  width="100%"
-  height="400" />
+  width="100%" />
